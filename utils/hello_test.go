276ac17847ed8ad2f99ee112a49a5a117d248427
chaewonkong <chaewonkong@nexon.co.kr>
2023-01-13 15:39:23 +0900

    test: add fail case

--- utils/hello_test.go
+++ utils/hello_test.go
@@ -3,7 +3,7 @@ package utils
 import "testing"
 
 func TestHell(t *testing.T) {
-	if res := Hello(); res != "hello" {
+	if res := Hello(); res == "hello" {
 		t.Errorf("expected: 'hello' but received: %v", res)
 	}
 }
