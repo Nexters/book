d5db8c5537c16901dc275fc3bfa6309e6fe9e31d
chaewonkong <chaewonkong@nexon.co.kr>
2023-01-25 15:59:08 +0900

    fix: column property

--- app/entity/book.go
+++ app/entity/book.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 type Book struct {
 	gorm.Model
-	Title       string `gorm:"title" json:"title"`
-	Author      string `gorm:"author" json:"author"`
-	ShopLink    string `gorm:"shopLink" json:"shopLink"`
-	Image       string `gorm:"image" json:"image"`
-	Price       string `gorm:"price" json:"price"`
-	Publisher   string `gorm:"publisher" json:"publisher"`
-	ISBN        string `gorm:"ISBN" json:"ISBN"`
-	Description string `gorm:"description" json:"description"`
+	Title       string `gorm:"column:title" json:"title"`
+	Author      string `gorm:"column:author" json:"author"`
+	ShopLink    string `gorm:"column:shopLink" json:"shopLink"`
+	Image       string `gorm:"column:image" json:"image"`
+	Price       string `gorm:"column:price" json:"price"`
+	Publisher   string `gorm:"column:publisher" json:"publisher"`
+	ISBN        string `gorm:"column:ISBN" json:"ISBN"`
+	Description string `gorm:"column:description" json:"description"`
 }
--- app/entity/user.go
+++ app/entity/user.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email string `gorm:"email" json:"email"`
-	Uid   string `gorm:"uid" json:"uid"`
+	Email string `gorm:"column:email" json:"email"`
+	Uid   string `gorm:"column:uid" json:"uid"`
 	Books []Book `gorm:"many2many:user_books;" json:"books"`
 }
