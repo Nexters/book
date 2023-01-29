6b51eff1958db724a80f8ebc7d72683e347a2784
chaewonkong <chaewonkong@gmail.com>
2023-01-29 22:22:07 +0900

    chore: add echo middleware

--- app/auth/bearer.go
+++ app/auth/bearer.go
@@ -2,16 +2,19 @@ package auth
 
 import (
 	"errors"
+	"net/http"
 	"regexp"
 	"strings"
 
 	"github.com/labstack/echo/v4"
+	"go.uber.org/fx"
 )
 
 type (
 	BearerAuth interface {
-		GetToken(c echo.Context, i interface{}) (string, error)
+		GetToken(c echo.Context) (string, error)
 		ParseToken(auth string) (string, error)
+		ValidateBearerHeader(next echo.HandlerFunc) echo.HandlerFunc
 	}
 
 	bearerAuth struct {
@@ -23,7 +26,23 @@ func NewBearerAuth() BearerAuth {
 	return bearerAuth{}
 }
 
-func (b bearerAuth) GetToken(c echo.Context, i interface{}) (token string, err error) {
+// middleware
+func (b bearerAuth) ValidateBearerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		binder := new(echo.DefaultBinder)
+		err := binder.BindHeaders(c, &b)
+
+		_, err = b.ParseToken(b.Authorization)
+
+		if err != nil {
+			return echo.NewHTTPError(http.StatusUnauthorized, "Provide bearer user token")
+		}
+		c.Response().Header().Set(echo.HeaderServer, "Echo/3.0")
+		return next(c)
+	}
+}
+
+func (b bearerAuth) GetToken(c echo.Context) (token string, err error) {
 	binder := new(echo.DefaultBinder)
 	err = binder.BindHeaders(c, &b)
 	if err != nil {
@@ -51,3 +70,5 @@ func (b bearerAuth) ParseToken(auth string) (token string, err error) {
 
 	return
 }
+
+var BearerAuthModuole = fx.Module("auth/bearer", fx.Provide(NewBearerAuth))
