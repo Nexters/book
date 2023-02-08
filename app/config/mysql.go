8d1712e2122b098c859939f6c591fbd18144938a
chaewonkong <chaewonkong@gmail.com>
2023-02-08 21:38:09 +0900

    fix: add parseTime true

--- app/config/mysql.go
+++ app/config/mysql.go
@@ -12,7 +12,7 @@ type MySQLDialector gorm.Dialector
 
 // NewMySQLDialector 생성자
 func NewMySQLDialector(settings *Settings) MySQLDialector {
-	dsn := fmt.Sprintf("%s:%s@tcp(%v:%v)/%v?", settings.Database.User, settings.Database.Password, settings.Database.URL, settings.Database.Port, settings.Database.Name)
+	dsn := fmt.Sprintf("%s:%s@tcp(%v:%v)/%v?parseTime=true", settings.Database.User, settings.Database.Password, settings.Database.URL, settings.Database.Port, settings.Database.Name)
 
 	return mysql.Open(dsn)
 }
