467edd80bcefb4dc2fa25f00c54c1d6e18ad8e00
chaewonkong <chaewonkong@gmail.com>
2023-02-09 21:17:40 +0900

    fix: include new domains to cors allow origins

--- app/app.go
+++ app/app.go
@@ -33,7 +33,7 @@ func RegisterHooks(
 			go func() {
 				e.Validator = validator
 				e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-					AllowOrigins:     []string{"http://localhost:3030", "http://localhost:3000"},
+					AllowOrigins:     []string{"http://localhost:3030", "http://localhost:3000", "https://pieceofbook.com", "https://www.pieceofbook.com"},
 					AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowCredentials, echo.HeaderAuthorization},
 					AllowCredentials: true,
 					AllowMethods:     []string{http.MethodGet, http.MethodOptions, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
