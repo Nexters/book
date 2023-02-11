98789ae136a0914c1afa82e6e968e6f8962d634e
chaewonkong <chaewonkong@gmail.com>
2023-02-11 15:58:57 +0900

    fix: 전체 메모 개수를 조회하게 user api 수정

--- app/service/user.go
+++ app/service/user.go
@@ -17,10 +17,12 @@ type (
 	}
 )
 
+// NewUserService 생성자
 func NewUserService(u repository.UserRepository, b BookService) UserService {
 	return userService{u, b}
 }
 
+// FindUserStat 사용자의 통계 조회
 func (u userService) FindUserStat(userID string) (stat payloads.UserStatPayload, err error) {
 	user, err := u.userRepo.FindUserByUID(userID)
 	if err != nil {
@@ -28,18 +30,22 @@ func (u userService) FindUserStat(userID string) (stat payloads.UserStatPayload,
 	}
 	start := user.CreatedAt
 
-	books, err := u.bookService.FindAllBooks(userID, false)
+	books_reading, err := u.bookService.FindAllBooks(userID, false)
 	if err != nil {
 		return
 	}
 
-	stat.ReadCount = books.Count
+	books_read, err := u.bookService.FindAllBooks(userID, false)
+	if err != nil {
+		return
+	}
+
+	stat.ReadCount = books_reading.Count
 	var end time.Time
-	for _, book := range books.Books {
+	for _, book := range books_reading.Books {
 		if end.Sub(book.UpdatedAt) < 0 {
 			end = book.UpdatedAt
 		}
-		stat.MemoCount += book.MemoCount
 	}
 
 	if end.Sub(start) < 0 {
@@ -47,7 +53,20 @@ func (u userService) FindUserStat(userID string) (stat payloads.UserStatPayload,
 		return
 	}
 
+	stat.MemoCount = countAllMemos(books_reading, books_read)
 	stat.Duration = int64(end.Sub(start).Hours() / 24)
+
+	return
+}
+
+// countAllMemos 읽는 중, 완독 모두 카운트
+func countAllMemos(readings payloads.FindAllBooksPayload, reads payloads.FindAllBooksPayload) (memoCount int) {
+	for _, reading := range readings.Books {
+		memoCount += reading.MemoCount
+	}
+	for _, read := range reads.Books {
+		memoCount += read.MemoCount
+	}
 
 	return
 }
