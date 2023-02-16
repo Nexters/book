2a383e369ecb2d790394b06c49494a656e2e80dc
chaewonkong <chaewonkong@gmail.com>
2023-02-16 19:08:15 +0900

    refactor: http/controller

--- http/controller.go
+++ http/controller.go
@@ -6,48 +6,26 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/nexters/book/app/book"
 
-	"github.com/nexters/book/http/auth"
-
 	"github.com/nexters/book/app/memo"
 	"github.com/nexters/book/app/user"
 	echoSwagger "github.com/swaggo/echo-swagger"
 	"go.uber.org/fx"
 )
 
-// Controller 컨트롤러 구조체
-type Controller struct {
-	Book book.BookController
-	User user.UserController
-	Memo memo.MemoController
-}
-
-// NewController 생성자
-func NewController(
-	book book.BookController,
-	user user.UserController,
-	memo memo.MemoController,
-) Controller {
-	return Controller{book, user, memo}
-}
-
-// bindRoute 라우팅
-func bindRoute(e *echo.Echo, c Controller, ba auth.BearerAuth) {
+// appRoute  health check, swagger 라우팅
+func appRoute(e *echo.Echo) {
 	e.GET("", func(c echo.Context) error {
 		return c.String(http.StatusOK, "ok")
 	})
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
-
 }
 
 // ControllerModule 컨트롤러
 var ControllerModule = fx.Module(
 	"controller",
-	fx.Provide(
-		NewController,
-	),
 	book.BookControllerModule,
 	user.UserControllerModule,
 	memo.MemoControllerModule,
-	fx.Invoke(bindRoute),
+	fx.Invoke(appRoute),
 )
